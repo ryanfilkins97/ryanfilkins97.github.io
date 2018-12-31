5a2047204a23d1e8a409ad8042eeda312fcc7a7a
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2018-12-31 16:40:51 -0600

    No longer single page for compatibility with GitHub hosting

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-var app = angular.module("portfolioApp", ["ngRoute"]); 
-
-app.config(function($routeProvider, $locationProvider){
-    $routeProvider.when("/", 
-                        {
-                            templateUrl: "about.html",
-                            title: "About"
-                        })
-                  .when("/education", 
-                        {
-                            templateUrl: "education.html",
-                            title: "Education"
-                        })
-                  .when("/experience", 
-                        {
-                            templateUrl: "experience.html",
-                            title: "Experience"
-                        })
-                  .when("/skills", 
-                        {
-                            templateUrl: "skills.html",
-                            title: "Skills"
-                        });
-});
-
-app.controller("controller", function($scope, $location){
-    var controller = $scope;
-    
-    controller.isActive = function(path) {
-        return path == $location.path();
-    };
-});
-
 $(function(){    
     M.AutoInit();
 });
